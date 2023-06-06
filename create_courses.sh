81fc4f087ea411651a868ef70cf06f73a40858d5
ssblackhorse <80287928+ssblackhorse@users.noreply.github.com>
2023-06-06 13:49:10 -0400

    Updates

---
+++ create_courses.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Prompt the user for the course name
+read -p "Enter the course name: " coursename
+
+# Define the base path
+basepath="/var/lib/docker/volumes/classes/$coursename"
+
+# Create the folders using sudo
+sudo mkdir -p "$basepath/uploads"
+sudo mkdir -p "$basepath/logs"
+sudo mkdir -p "$basepath/redis/data"
+sudo mkdir -p "$basepath/db/mysql"
+
+# Change ownership of the folders to the current user
+sudo chown -R $USER:$USER "$basepath"
+
+# Copy and rename the configuration file
+sudo cp "$PWD/config_files/course.subdomain.conf" "/var/lib/docker/swag/config/proxy-confs/$coursename.subdomain.conf"
+
+# Replace <container_name> with the course name in the copied file
+sudo sed -i "s/<container_name>/$coursename/g" "/var/lib/docker/swag/config/proxy-confs/$coursename.subdomain.conf"
+
+# Restart the 'swag' Docker container
+sudo docker restart swag
+
+# Display success message
+echo "Folder structure created, configuration file copied, and 'swag' Docker container restarted successfully."
+
+# Prompt for DNS configuration
+echo "Please make sure that you have configured your DNS to have a CNAME record which points to ${coursename}.yourdomain.tld."
---
+++ setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Check if Docker is installed
+if ! [ -x "$(command -v docker)" ]; then
+    echo "Docker is not installed. Installing Docker..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
+    echo "Docker has been installed."
+else
+    echo "Docker is already installed."
+fi
+
+# Check if Docker Compose is installed
+if ! [ -x "$(command -v docker-compose)" ]; then
+    echo "Docker Compose is not installed. Installing Docker Compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    echo "Docker Compose has been installed."
+else
+    echo "Docker Compose is already installed."
+fi
+
+# Post-installation recommendations
+echo "Performing post-installation steps..."
+# Enable Docker to start on boot
+sudo systemctl enable docker
+# Start Docker service
+sudo systemctl start docker
+# Add current user to the 'docker' group
+sudo usermod -aG docker $USER
+# Display Docker version
+docker --version
+# Display Docker Compose version
+docker-compose --version
+echo "Post-installation steps have been completed."
+
+# Create Docker volumes and networks
+docker volume create swag
+docker volume create classes
+docker volume create ctfd
+docker volume create portainer_data
+docker network create swag_default
+
+# Check if volumes are created successfully
+if [ $? -eq 0 ]; then
+  echo "Docker volumes created successfully."
+else
+  echo "Failed to create Docker volumes."
+fi
+
+# Clone CTFd repository
+echo "Cloning CTFd repository to the ctfd volume..."
+git clone https://github.com/CTFd/CTFd.git /var/lib/docker/volumes/ctfd/
+echo "CTFd repository cloned successfully."
+
+
+# Add dockeruser and grant sudo privileges
+echo "Creating the docker user and updating it's permissions..."
+sudo adduser dockeruser
+sudo usermod --password $(openssl rand -base64 12) dockeruser
+sudo adduser dockeruser sudo
+
+# Add dockeruser to the docker group
+sudo usermod -aG docker dockeruser
+
+# Switch to the docker group
+newgrp docker
+
+echo "New user details:"
+echo "Username: dockeruser"
+echo "Password: $(sudo cat /etc/shadow | grep dockeruser | awk -F':' '{print $2}')"

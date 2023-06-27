3eced954629c54b2c958de914fb4d8c01ecfa1ee
ssblackhorse <80287928+ssblackhorse@users.noreply.github.com>
2023-06-27 10:30:17 -0400

    Update create_courses.sh

--- setup/create_courses.sh
+++ setup/create_courses.sh
@@ -16,10 +16,14 @@ sudo mkdir -p "$basepath/db/mysql"
 sudo chown -R $USER:$USER "$basepath"
 
 # Copy and rename the configuration file
-sudo cp "/root/config_files/course.subfolder.conf" "/var/lib/docker/swag/config/proxy-confs/$coursename.subfolder.conf"
+sudo cp "../config_files/courses.subfolder.conf" "../config_files/$coursename.subfolder.conf"
 
 # Replace <container_name> with the course name in the copied file
-sudo sed -i "s/<container_name>/$coursename/g" "/var/lib/docker/swag/config/proxy-confs/$coursename.subfolder.conf"
+sudo sed -i "s/<container_name>/$coursename/g" "../config_files/$coursename.subfolder.conf"
+
+# Move the new conf to the proxy-confs directory
+sudo mv -i "../config_files/$coursename.subfolder.conf" "/var/lib/docker/volumes/swag/config/nginx/proxy-confs"
+sudo ls /var/lib/docker/volumes/swag/config/nginx/proxy-confs | grep $coursename
 
 # Restart the 'swag' Docker container
 sudo docker restart swag
@@ -28,4 +32,4 @@ sudo docker restart swag
 echo "Folder structure created, configuration file copied, and 'swag' Docker container restarted successfully."
 
 # Prompt for DNS configuration
-echo "Please make sure that you have configured your DNS to have a CNAME record which points to https://yourdomain.tld/${coursename}."
+# echo "Please make sure that you have configured your DNS to have a CNAME record which points to https://yourdomain.tld/${coursename}."
